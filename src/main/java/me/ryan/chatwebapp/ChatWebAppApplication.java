2ed303d9fbf898e49c42adf6db2825dbc7c3f534
mogongzi <superrenyuan@hotmail.com>
2021-01-17 22:57:46 +0800

    build sample web app for chatting by using spring boot

---
+++ src/main/java/me/ryan/chatwebapp/ChatWebAppApplication.java
@@ -0,0 +1,13 @@
+package me.ryan.chatwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ryan/chatwebapp/ChatWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ryan.chatwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
