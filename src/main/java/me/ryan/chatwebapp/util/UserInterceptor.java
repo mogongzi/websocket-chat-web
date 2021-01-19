72e9f665ccf57ad2280cabe61bb21f49cb33b779
mogongzi <superrenyuan@hotmail.com>
2021-01-19 11:17:31 +0800

    change user raw type from LinkedList to ArrayList

--- src/main/java/me/ryan/chatwebapp/util/UserInterceptor.java
+++ src/main/java/me/ryan/chatwebapp/util/UserInterceptor.java
@@ -2,28 +2,29 @@ package me.ryan.chatwebapp.util;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.messaging.support.NativeMessageHeaderAccessor;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 
 public class UserInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
+        if (StompCommand.CONNECT.equals(Objects.requireNonNull(accessor).getCommand())) {
+            Object raw = message.getHeaders().get(NativeMessageHeaderAccessor.NATIVE_HEADERS);
 
             if (raw instanceof Map) {
-                Object name = ((Map) raw).get("username");
+                Object name = ((Map<?, ?>) raw).get("username");
 
-                if (name instanceof LinkedList) {
-                    accessor.setUser(new User(((LinkedList) name).get(0).toString()));
+                if (name instanceof ArrayList) {
+                    accessor.setUser(new User(((ArrayList<?>) name).get(0).toString()));
                 }
             }
         }
